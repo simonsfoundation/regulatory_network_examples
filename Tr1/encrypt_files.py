da29b73eef94db22f5cb15dc1e889a967b8ad25f
Aaron Watters <awatters@simonsfoundation.org>
2015-12-04 14:28:55 -0500

    Add Tr1 files encrypted differently.

---
+++ Tr1/encrypt_files.py
@@ -0,0 +1,98 @@
+"""
+Usage:
+
+Encode using password:
+% python encrypt_files.py encode password
+
+Decode using password:
+% python encrypt_files.py decode password
+
+Encode files and delete unencrypted files:
+# python encrypt_files.py delete password
+"""
+
+import os
+import glob
+from Crypto.Cipher import ARC4
+
+dont_encrypt = ".ipynb .crypt .py ~".split()
+
+def get_encoder(password):
+    return ARC4.new(password)
+
+def encrypt(password, directory=".", delete=False):
+    for filename in os.listdir(directory):
+        if os.path.isdir(filename):
+            continue # skip directories
+        doit = True
+        for suffix in dont_encrypt:
+            if filename.endswith(suffix):
+                doit = False
+        if doit:
+            outname = filename+".crypt"
+            print("encrypting "+filename+" to "+outname)
+            encoder = get_encoder(password)
+            text = open(filename, "rb").read()
+            encrypted = encoder.encrypt(text)
+            outfile = open(outname, "wb")
+            outfile.write(encrypted)
+            outfile.close()
+            if delete:
+                print("deleting "+filename)
+                os.unlink(filename)
+
+def decrypt(password, directory="."):
+    for filename in os.listdir(directory):
+        if filename.endswith(".crypt"):
+            outname = filename[:-6]
+            print("decrypting "+filename+" to "+outname)
+            encrypted = open(filename, "rb").read()
+            encoder = get_encoder(password)
+            decrypted = encoder.decrypt(encrypted)
+            if os.path.exists(outname):
+                text = open(outname, "rb").read()
+                if text==decrypted:
+                    print("Existing file matches "+outname)
+                else:
+                    print("EXISTING FILE DOES NOT MATCH "+outname)
+            else:
+                outfile = open(outname, "wb")
+                outfile.write(decrypted)
+                outfile.close()
+                print("wrote "+outname)
+
+def decrypt_widget():
+    import time
+    from jp_gene_viz import js_proxy
+    js_proxy.load_javascript_support()
+    # wait for the js to compile...
+    time.sleep(0.5)
+    from IPython.display import display
+    d = js_proxy.ProxyWidget()
+    elt = d.element()
+    #display(d)
+    window = d.window()
+    jQuery = window.jQuery
+    d(elt.width("400px").height("100px"))
+    d(elt.html("""<input type="password" id="pw000"/> <input id="b000" type="button" value="decrypt files"/> """))
+    def click_handler(id, args):
+        password = d.evaluate(jQuery("#pw000").val())
+        # print("pw="+repr(password))
+        decrypt(password)
+    callback = d.callback(click_handler, None)
+    d(jQuery("#b000").click(callback))
+    d.flush()
+    #display(d)
+    return d
+
+if __name__ == "__main__":
+    import sys
+    [mode, password] = sys.argv[1:]
+    if mode == "encode":
+        encrypt(password)
+    elif mode == "delete":
+        encrypt(password, delete=True)
+    elif mode == "decode":
+        decrypt(password)
+    else:
+        print __doc__
